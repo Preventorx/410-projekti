3a5ada3917b9a182ae26088bf4c20746a6159236
Preventorx <preventorx@gmail.com>
2025-06-23 11:03:31 +0300

    Videon harjoittelua

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+let lomake = document.forms["formNewItem"];
+lomake.addEventListener("submit", uusiListaElementti)
+
+function uusiListaElementti(event){
+
+    event.preventDefault()
+    
+    //Haetaan elementille nimi 
+    let elementinNimi = document.querySelector('#main input[type="text"]').value;
+
+    //Luodaan uusi li-elementti ja sille sisältö
+    let uusiElementti = document.createElement("li")
+    let uusiElementtiTeksti = document.createTextNode(elementinNimi)
+    uusiElementti.appendChild(uusiElementtiTeksti)
+
+    //Lisätään list-item class
+    uusiElementti.ClassName = "list-item";
+
+    //Lisätään uusi li-elementti oikeaan paikkaan
+    document.querySelector("#itemList").appendChild(uusiElementti)
+}
