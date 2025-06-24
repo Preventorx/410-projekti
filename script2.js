6cf1f3ff3c8c3c6693039a26393f49ebe31f5a53
Preventorx <preventorx@gmail.com>
2025-06-24 11:14:29 +0300

    Versio 2 javascriptiä

--- script2.js
+++ script2.js
@@ -1,4 +1,19 @@
-function toiminto() {
-    let kaveri = document.getElementById("input#nimi");
+let kaverilista = document.forms("kaverilista");
+kaverilista.addEventListener("submit", toiminto)
 
+
+function toiminto(event) {
+    event.preventDefault()
+
+    const kaverit = [];
+    let kaveri = document.getElementById("nimi");
+    if document.getElementById("nimi").onclick == "lisää" {
+        kaverit.push(kaveri);       
+        let list = document.getElementById("lista");
+        for (let i = 0; i < persons.length; ++i) {
+            let li = document.createElement("li");
+            li.innerText = persons[i];
+            list.appendChild(li);
+        }
+    }
 }
