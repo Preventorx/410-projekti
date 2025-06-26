a42dc51bafc4d504a1ed36607ab2e1adf072cabc
Preventorx <preventorx@gmail.com>
2025-06-26 06:10:31 +0300

    Saatu versio 2 loppuun

--- script2.js
+++ script2.js
@@ -1,27 +1,37 @@
 let kaverilista = document.forms["kaverilista"];
+let lista = document.getElementById("lista");
 
 function lisaa() {
     const kaverit = [];
     let kaveri = document.querySelector("#kaverilista input[type='text']").value; 
-
     kaverit.push(kaveri);
 
-    let list = document.getElementById("lista");
     for (let i = 0; i < kaverit.length; ++i) {
         let li = document.createElement("li");
         li.innerText = kaverit[i];
-        list.appendChild(li);
+        lista.appendChild(li);
     }
-}
 
+}
 
 function poista() {
     let kaveri = document.querySelector("#kaverilista input[type='text'").value;
-    let list = document.getElementById("lista");
-          
+    let nimet = lista.getElementsByTagName("li");
+
+    for (let i = nimet.length - 1; i >= 0; i--) {
+        let nimi = nimet[i].textContent;
+        if (nimi === kaveri) {
+            lista.removeChild(nimet[i]);
+        }
+    }
 }
 
-
 function jarjesta() {
+    let nimet = Array.from(lista.getElementsByTagName("li"));
 
+    nimet.sort((a, b) =>
+        a.textContent.localeCompare(b.textContent));
+
+    lista.innerHTML = "";
+    nimet.forEach(nimi => lista.appendChild(nimi));
 }
